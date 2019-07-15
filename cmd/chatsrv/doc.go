096a5d2bc97362513d7f5ce3a6d63e7ba42681b4
Mike Mikhailov <webtask@gmail.com>
2019-07-15 23:31:01 +0300

    docs

--- cmd/chatsrv/doc.go
+++ cmd/chatsrv/doc.go
@@ -1,2 +1,13 @@
-// Package server implements TCP chat server application.
+// Package `chatsrv` implements server application for chat over TCP.
+// 
+// To compile chat server locally, run from package directory:
+//
+//	go install .
+// 
+// Server binary `chatsrv` will be placed into bin/ directory under Go projets root,
+// identified with GOPATH environment variable.
+// 
+// Or quickly launch server with command:
+// 
+//	go run .
 package main
